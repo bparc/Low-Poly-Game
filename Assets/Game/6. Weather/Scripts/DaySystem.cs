3f50632d667e9e47256a7e8a8b2548401249eaba
RhAnjiE <iRhAnjiE@gmail.com>
2018-03-26 01:04:57 +0200

    Added first version of Day/Night Cycle

--- Assets/Game/6. Weather/Scripts/DaySystem.cs
+++ Assets/Game/6. Weather/Scripts/DaySystem.cs
@@ -3,83 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DaySystem : MonoBehaviour {
+    public Material skyboxDay;
+    public Material skyboxNight;
 
-    /// TODO (by RhAnjiE) - "Rewrite this system, currently doesn't work properly"
-    private void Update(){
-        GUI.Label(new Rect(20, 0, 300, 30), "Script 'DaySystem' doesn't working currently. It need rework!");
-    }
+    public Light sunLight;
 
-     /*public Light sunLight;
-     public Light moonLight;
-     public GameObject stars_ = null;
-
+    [Header("Velocity")]
     public float minutesPerCycle = 1f;
+    public float speedIntensity = 0.2f;
 
-     public float minIntensity = 0f;
-     public float maxIntensity = 1f;
+    [Header("Min-Max Settings")]
+    public float minIntensity = 0f;
+    public float maxIntensity = 1f;
 
-     public float minAmbient = 0f;
-     public float maxAmbient = 1f;
+    public float minAmbient = 0f;
+    public float maxAmbient = 1f;
 
-     public float minFlare = 0f;
-     public float maxFlare = 1f;
 
-    private float actualRotate, timeDay;
+    private float actualRotate, timeDay = 0f;
 
-     private LensFlare sunFlare;
-
-     const float minute = 60;
-     const float hour = minute * 60;
-     const float all = hour * 24;
-     const float halfCycle = 30f;
-
-    const float degreesPerSecond = 360 / all;
-
+    const float allSecondsInDay = (60 * 60 * 24);
+    const float degreesPerSecond = 360 / allSecondsInDay;
+    const float fullCycle = 60f;
 
 	void Start () {
-        timeDay = 0f;
-        //this.transform.eulerAngles.x = 0f;
-
-        sunFlare = sunLight.GetComponent<LensFlare>();
-    }
-
-    private void Update() {
-        actualRotate = degreesPerSecond * all / (minutesPerCycle * minute);
-
+		
+	}
+	
+	void Update () {
+        actualRotate = degreesPerSecond * allSecondsInDay / (minutesPerCycle * 60);
         this.transform.Rotate(new Vector3(actualRotate, 0, 0) * Time.deltaTime);
+
         timeDay += Time.deltaTime / minutesPerCycle;
+        if (timeDay >= fullCycle) timeDay = 0;
 
-        if (timeDay >= halfCycle*2) timeDay = 0;
-
-        if (timeDay > 25 && timeDay < halfCycle) { //ZMIERZCH
-            if(sunLight.intensity > minIntensity)
-             sunLight.intensity -= 0.3f * Time.deltaTime;
+        if (timeDay > fullCycle / 2 && timeDay < fullCycle / 2 + 10) {
+            if (sunLight.intensity > minIntensity)
+                sunLight.intensity -= speedIntensity * Time.deltaTime;
 
             if (RenderSettings.ambientIntensity > minAmbient)
-             RenderSettings.ambientIntensity -= 0.3f * Time.deltaTime;
+                RenderSettings.ambientIntensity -= speedIntensity * Time.deltaTime;
 
-            if(sunFlare.brightness > minFlare && timeDay>28.5)
-             sunFlare.brightness -= 1f * Time.deltaTime;        
+            RenderSettings.skybox = skyboxNight;
+            DynamicGI.UpdateEnvironment();
         }
 
-        else if(timeDay>55 || timeDay < 10) { //SWIT
-            if(sunLight.intensity < maxIntensity)
-             sunLight.intensity += 0.3f * Time.deltaTime;
+        else
+        if (timeDay > fullCycle - 5 || timeDay < 10) {
+            if (sunLight.intensity < maxIntensity)
+                sunLight.intensity += speedIntensity * Time.deltaTime;
 
-            if(RenderSettings.ambientIntensity < maxAmbient)
-             RenderSettings.ambientIntensity += 0.3f * Time.deltaTime;
+            if (RenderSettings.ambientIntensity < maxAmbient)
+                RenderSettings.ambientIntensity += speedIntensity * Time.deltaTime;
 
-            if(sunFlare.brightness < maxFlare && timeDay < 5)
-             sunFlare.brightness += 0.4f * Time.deltaTime;
+            RenderSettings.skybox = skyboxDay;
+            DynamicGI.UpdateEnvironment();
         }
 
+        Debug.Log($"{fullCycle / 2 + 10}");
+	}
+
+
+    private void OnGUI() {
+        System.TimeSpan timeSpan = System.TimeSpan.FromHours(timeDay / fullCycle * 24);
+
+        GUI.Label(new Rect(20, 0, 300, 20), $"Czas: {timeSpan.Hours + 6}:{timeSpan.Minutes}");
+        GUI.Label(new Rect(20, 20, 300, 20), $"Intensywnosc: {RenderSettings.ambientIntensity}");
     }
-
-
-    private void OnGUI(){ //DEBUG
-        System.TimeSpan timeSpan = System.TimeSpan.FromDays(timeDay / 100.0f);
-
-        GUI.Label(new Rect(20, 0, 300, 30), "Czas: " + $"{timeSpan.Hours}:{timeSpan.Minutes}"
-            + " ; Intensity: " + sunLight.intensity.ToString("0.0") + " ; Brightness: " + sunFlare.brightness.ToString("0.0"));
-    }*/
 }
